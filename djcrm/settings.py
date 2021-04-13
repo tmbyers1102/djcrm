6c1a192200d3f155a3edf6b3c52b029b9279e744
Tom Byers <tmbyers1102@gmail.com>
2021-04-13 10:25:12 -0700

    added "*" to line 20 ALLOWED_HOSTS in settings.py

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
