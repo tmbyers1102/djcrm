5b2c2d7ce1661191032184409bdf2151ebb845d9
Tom Byers <tmbyers1102@gmail.com>
2021-04-13 13:22:07 -0700

    moved whitenoise to bottom of INSTALLED_APPS

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -23,13 +23,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+
     
     # 3rd Party Apps
     'crispy_forms',
